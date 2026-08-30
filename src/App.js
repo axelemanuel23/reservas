f70af898fb7f75c74dda38ca2cf20409f0b2465c
Axel Emanuel <100535827+axelemanuel23@users.noreply.github.com>
2026-08-30 17:46:32 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ function sortBoothsForAssignment(booths) {
   return [...entrada, ...salida];
 }
 
-const INITIAL_AGENTS = [{}];
+const INITIAL_AGENTS = [{ id: 1, name: "" }];
 
 const INITIAL_DEMAND = [
   {
